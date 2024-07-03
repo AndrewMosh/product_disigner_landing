670cc04161110aea180015c704ed0778463eff3a
Andrew Mosh <malinaenglishclub@gmail.com>
2024-07-03 16:39:48 +0300

    loader removed

--- src/App.js
+++ src/App.js
@@ -6,40 +6,23 @@ import Experience from "./Components/Experience/Experience";
 import Skills from "./Components/Skills/Skills";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import ContactButton from "./Components/ContactButton/ContactButton";
-import Loader from "./Components/Loader/Loader";
-import { useState, useEffect } from "react";
-function App() {
-  const [loadingProgress, setLoadingProgress] = useState(0);
-  useEffect(() => {
-    // Simulate loading progress for demonstration purposes
-    const interval = setInterval(() => {
-      setLoadingProgress((prevProgress) => prevProgress + 0.02);
-    }, 70);
 
-    return () => clearInterval(interval);
-  }, []);
-  return (
-    <div className="App">
-      {loadingProgress < 1 ? (
-        <Loader loadingProgress={loadingProgress} />
-      ) : (
-        // Content to show after loading is complete
-        <>
-          <Main />
-          <Gap props="Я" />
-          <About />
-          <Gap props="Опыт" />
-          <Experience />
-          <Gap props="Навыки" />
-          <Skills />
-          <Gap props="Портфолио" />
-          <Portfolio />
-          <Gap props="Footer" />
-          <ContactButton />
-        </>
-      )}
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <Main />
+            <Gap props="Я" />
+            <About />
+            <Gap props="Опыт" />
+            <Experience />
+            <Gap props="Навыки" />
+            <Skills />
+            <Gap props="Портфолио" />
+            <Portfolio />
+            <Gap props="Footer" />
+            <ContactButton />
+        </div>
+    );
 }
 
 export default App;
